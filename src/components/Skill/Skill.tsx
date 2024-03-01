11d4e24f34c5bf3fa06c36ec40a5a29d7ec1774a
Reva Enad <reva.enad@jg-corporation.com>
2024-03-01 15:45:39 +0800

    skills added

--- src/components/Skill/Skill.tsx
+++ src/components/Skill/Skill.tsx
@@ -1,27 +1,117 @@
-import './skill.css'
+import React from 'react';
+import { Card, CardContent, Typography, Grid, Icon } from '@mui/material';
+import { Verified } from '@mui/icons-material'; 
+import './skill.css';
 
 function Skill() {
   return (
-
     <div>
-    <section id='skill'>
-    <div className="skill-container">
-        <div className="skill-content">
-          <h3 className="skill-heading fraunces">Hello. Welcome!</h3>
-          <p className="skill-subheading poppins-regular"> I'm Reva Enad</p>
-          <div className="button-container">
-            <div className="resume-container">
-             
-            </div>
-            <div className="contact-button-container">
-             
-            </div>
+      <section id='skill'>
+        <div className="skill-container">
+          <div className="skill-content">
+            <h4 className="skill-heading fraunces">What Skills I Have</h4>
+            <Grid container spacing={4}> 
+              <Grid item xs={4}>
+                <Card className='resume-container'>
+                  <CardContent>
+                    <Typography gutterBottom variant='h6' component='h2'>
+                      Frontend Development
+                    </Typography>
+                    <Grid container spacing={1} alignItems="center" justifyContent="space-between">
+                      {/* First Group */}
+                      <Grid container item spacing={1} xs={6} alignItems="center">
+                        <Grid item>
+                          <Icon color="primary">
+                            <Verified fontSize="small" />
+                          </Icon>
+                        </Grid>
+                        <Grid item>
+                          <Typography variant="body2" color="white">HTML</Typography>
+                        </Grid>
+                      </Grid>
+                      {/* Second Group */}
+                      <Grid container item spacing={1} xs={6} alignItems="center">
+                        <Grid item>
+                          <Icon color="primary">
+                            <Verified fontSize="small" />
+                          </Icon>
+                        </Grid>
+                        <Grid item>
+                          <Typography variant="body2" color="white">CSS</Typography>
+                        </Grid>
+                      </Grid>
+                      {/* 3rd Group */}
+                      <Grid container item spacing={1} xs={6} alignItems="center">
+                        <Grid item>
+                          <Icon color="primary">
+                            <Verified fontSize="small" />
+                          </Icon>
+                        </Grid>
+                        <Grid item>
+                          <Typography variant="body2" color="white">Bootstrap</Typography>
+                        </Grid>
+                      </Grid>
+                      {/* 4th Group */}
+                      <Grid container item spacing={1} xs={6} alignItems="center">
+                        <Grid item>
+                          <Icon color="primary">
+                            <Verified fontSize="small" />
+                          </Icon>
+                        </Grid>
+                        <Grid item>
+                          <Typography variant="body2" color="white">React</Typography>
+                        </Grid>
+                      </Grid>
+                    </Grid>
+                  </CardContent>
+                </Card>
+              </Grid>
+              <Grid item xs={4}>
+                <Card className='resume-container' >
+                  <CardContent>
+                    <Typography gutterBottom variant='h6' component='h2'>
+                      Backend Development
+                    </Typography>
+                    <Grid container spacing={1} alignItems="center">
+                      <Grid item>
+                        <Icon color="primary">
+                          <Verified fontSize="small" />
+                        </Icon>
+                      </Grid>
+                      <Grid item>
+                        <Typography variant="body2" color="textSecondary">HTML</Typography>
+                      </Grid>
+                    </Grid>
+                  </CardContent>
+                </Card>
+              </Grid>
+              <Grid item xs={4}>
+                <Card className='resume-container'>
+                  <CardContent>
+                    <Typography gutterBottom variant='h6' component='h2'>
+                      Database Management
+                    </Typography>
+                    <Grid container spacing={1} alignItems="center">
+                      <Grid item>
+                        <Icon color="primary">
+                          <Verified fontSize="small" />
+                        </Icon>
+                      </Grid>
+                      <Grid item>
+                        <Typography variant="body2" color="textSecondary">HTML</Typography>
+                      </Grid>
+                    </Grid>
+                  </CardContent>
+                </Card>
+              </Grid>
+            </Grid>
+          </div>
+          <div className="contact-button-container">
           </div>
         </div>
+      </section>
     </div>
-    </section>
-    </div>
-  )
+  );
 }
 
-export default Skill
+export default Skill;
