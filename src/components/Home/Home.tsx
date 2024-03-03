09c3ea10bff3d67c77e76d082574bef8198df0f9
Reva Enad <reva.enad@gmail.com>
2024-03-03 13:00:21 +0800

    added socials

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,11 +1,12 @@
 import './home.css'; 
 import Resume from './Resume';
 import ContactButton from './ContactButton';
+import { LinkedIn, Email, GitHub } from '@mui/icons-material';
 
 const Home = () => {
   return ( 
     <section id='home'>
-    <div className="home-container">
+      <div className="home-container">
         <div className="home-content">
           <h3 className="home-heading fraunces">Hello. Welcome!</h3>
           <p className="home-subheading poppins-regular"> I'm Reva Enad.</p>
@@ -18,8 +19,20 @@ const Home = () => {
               <ContactButton />
             </div>
           </div>
+          <div className="social-icons-container">
+            <a href="https://www.linkedin.com/in/enadreva" target="_blank" rel="noopener noreferrer">
+              <LinkedIn />
+            </a>
+            <a href="mailto:reva.enad@gmail.com" target="_blank" rel="noopener noreferrer">
+              <Email />
+            </a>
+            <a href="https://github.com/RevaEnad" target="_blank" rel="noopener noreferrer">
+              <GitHub />
+            </a>
+          
+          </div>
         </div>
-    </div>
+      </div>
     </section>
   );
 };
