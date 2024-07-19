08b51504b202ff7a4fe3087529859c622a16c030
enadreva <reva.enad@jg-corporation.com>
2024-07-19 23:34:42 +0800

    x

--- src/components/WorkExperience/WorkExperience.tsx
+++ src/components/WorkExperience/WorkExperience.tsx
@@ -1,6 +1,6 @@
 import { Card, CardContent, Typography } from '@mui/material';
 import { WorkOutline } from '@mui/icons-material';
-import './workExperience.css';
+import './workexperience.css';
 
 interface Experience {
   company: string;
